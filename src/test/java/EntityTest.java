190c132e05a6c9f7164a92a85c026400b56817fa
palinaboczkowska <palina.boczkowska@gmail.com>
2023-10-09 12:58:56 +0200

    Change EntityTest.java and Entity.java

--- src/test/java/EntityTest.java
+++ src/test/java/EntityTest.java
@@ -1,14 +1,17 @@
 package src.test.java;
 
 import org.junit.jupiter.api.Test;
+import src.main.java.Entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EntityTest {
 
     @Test
     public void testEntityHealth(){
-      Entity entity1 = new Entity(one, 100);
-      assertEquals(100, entity.getHealth());
-      blabla
+      Entity entity1 = new Entity("one", 100);
+      assertEquals(100, entity1.getHealth());
+
     }
 
 
