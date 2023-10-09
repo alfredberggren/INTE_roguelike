526cebf580682338736e11fadf94af4e6035cfa3
palinaboczkowska <palina.boczkowska@gmail.com>
2023-10-09 12:47:17 +0200

    Change EntityTest.java

--- src/test/java/EntityTest.java
+++ src/test/java/EntityTest.java
@@ -1,4 +1,14 @@
 package src.test.java;
 
+import org.junit.jupiter.api.Test;
+
 public class EntityTest {
+
+    @Test
+    public void testEntityHealth(){
+      Entity entity1 = new Entity(one, 100);
+      assertEquals(100, entity.getHealth());
+    }
+
+
 }
