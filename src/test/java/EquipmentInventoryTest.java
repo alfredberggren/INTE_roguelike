90275b7882d5dfcd5ea99c26e5e50804b82ddda5
palinaboczkowska <palina.boczkowska@gmail.com>
2023-10-09 20:36:53 +0200

    change EquipmentInventory.java, EquipmentInventoryTest.java

--- src/test/java/EquipmentInventoryTest.java
+++ src/test/java/EquipmentInventoryTest.java
@@ -6,6 +6,19 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EquipmentInventoryTest {
+    @Test
+    @DisplayName("Testar att ta bort all equipment")
+    public void testRemoveAllEquipment() {
+        Equipment e1 = new Equipment("test1", Equipment.Effect.SPEED, 15);
+        Equipment e2 = new Equipment("test2", Equipment.Effect.HEALTH, 60);
+        Equipment e3 = new Equipment("test3", Equipment.Effect.DAMAGE, 100);
+        EquipmentInventory inventory= new EquipmentInventory();
+        inventory.add(e1);
+        inventory.add(e2);
+        inventory.add(e3);
+        inventory.removeAllEquipments();
+        assertEquals("", inventory.toString());
+    }
 
     @Test
     @DisplayName("Testar att lägga till equipment")
@@ -13,11 +26,11 @@ public class EquipmentInventoryTest {
         Equipment e1 = new Equipment("test1", Equipment.Effect.SPEED, 15);
         Equipment e2 = new Equipment("test2", Equipment.Effect.HEALTH, 60);
         Equipment e3 = new Equipment("test3", Equipment.Effect.DAMAGE, 100);
-        EquipmentInventory ei= new EquipmentInventory();
-        ei.add(e1);
-        ei.add(e2);
-        ei.add(e3);
-        assertEquals("test +15% Speed\ntest2 +60% Health\ntest3 +100% Damage\n", ei.toString());
+        EquipmentInventory inventory= new EquipmentInventory();
+        inventory.add(e1);
+        inventory.add(e2);
+        inventory.add(e3);
+        assertEquals("test +15% Speed\ntest2 +60% Health\ntest3 +100% Damage\n", inventory.toString());
     }
 
     @Test
@@ -26,13 +39,24 @@ public class EquipmentInventoryTest {
         Equipment e1 = new Equipment("test1", Equipment.Effect.SPEED, 15);
         Equipment e2 = new Equipment("test2", Equipment.Effect.HEALTH, 60);
         Equipment e3 = new Equipment("test3", Equipment.Effect.DAMAGE, 100);
-        EquipmentInventory ei= new EquipmentInventory();
-        ei.add(e1);
-        ei.add(e2);
-        ei.add(e3);
-        ei.drop(e1);
-        ei.drop(e2);
-        assertEquals("test3 +100% Damage\n", ei.toString());
+        EquipmentInventory inventory= new EquipmentInventory();
+        inventory.add(e1);
+        inventory.add(e2);
+        inventory.add(e3);
+        inventory.drop(e1);
+        inventory.drop(e2);
+        assertEquals("test3 +100% Damage\n", inventory.toString());
+
+    }
+
+    @Test
+    @DisplayName ("Testar att hämta equipment")
+    public void testGetEquipment(){
+        Equipment e = new Equipment("test", Equipment.Effect.HEALTH, 60);
+        EquipmentInventory inventory = new EquipmentInventory();
+        inventory.add(e);
+        Equipment equipment = inventory.getEquipment(0);
+        assertEquals("test + 60% Health\n", equipment.toString());
 
     }
 }
