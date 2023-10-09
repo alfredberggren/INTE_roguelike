468aa458a66f9627e63ee7a8d5deb52866f124f6
Alfred Berggren <alfred.berggren@gmail.com>
2023-10-09 13:32:46 +0200

    added getPosition method

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -1,20 +1,27 @@
 package src.main.java;
+import src.main.java.Position;
 
 public class Entity {
 
     private String name;
     private int health;
     private int speed;
+    private Position pos;
 
     public Entity(String name, int health, int speed){
         this.name = name;
         this. health = health;
         this.speed = speed;
+        this.pos = new Position(0, 0);
     }
 
 
     public String getName(){
         return name;
+    }
+
+    public Position getPosition() {
+        return pos;
     }
     public int getHealth() {
         return health;
