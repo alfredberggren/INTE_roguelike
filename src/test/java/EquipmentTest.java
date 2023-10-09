1fd500cfbaec54e163420859ac1a9557b645884e
annastrombeerg <annastromberg03@yahoose>
2023-10-09 13:31:43 +0200

    change Equipment.java and EquipmentTest.java

--- src/test/java/EquipmentTest.java
+++ src/test/java/EquipmentTest.java
@@ -1,5 +1,4 @@
-package src.test.java;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import src.main.java.Equipment;
 
@@ -8,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class EquipmentTest {
 
     @Test
+    @DisplayName("Testar att en equipment har ett namn")
     public void testEquipmentName() {
         Equipment e = new Equipment("test", 100, 10);
         assertEquals("test", e.getName());
