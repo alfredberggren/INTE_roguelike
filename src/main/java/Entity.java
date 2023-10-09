5d9c1065d6adf98132c90052f87a9c583019c8ec
palinaboczkowska <palina.boczkowska@gmail.com>
2023-10-09 13:25:59 +0200

    change Entity.java, Equipment.java, EquipmentTest.java

--- src/main/java/Entity.java
+++ src/main/java/Entity.java
@@ -4,14 +4,23 @@ public class Entity {
 
     private String name;
     private int health;
+    private int speed;
 
-    public Entity(String name, int health){
+    public Entity(String name, int health, int speed){
         this.name = name;
         this. health = health;
+        this.speed = speed;
     }
 
 
+    public String getName(){
+        return name;
+    }
     public int getHealth() {
         return health;
+    }
+
+    public int getSpeed() {
+        return speed;
     }
 }
--- src/main/java/Equipment.java
+++ src/main/java/Equipment.java
@@ -1,10 +1,12 @@
+package src.main.java;
+
 public class Equipment {
 
     private String name;
     private int effect;
     private int damage;
 
-    Equipment(String name, int effect, int damage) {
+    public Equipment(String name, int effect, int damage) {
         this.name = name;
         this.effect = effect;
         this.damage = damage;
