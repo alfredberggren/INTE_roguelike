b9b0cfb69eff640d7ee9f8e6fa825bf2f1f35baf
annastrombeerg <annastromberg03@yahoose>
2023-10-10 11:47:06 +0200

    La till toString metod i Physical + Ability + Magic, testerna testar nu ifall toStringen matchar

--- src/test/java/AbilityTest.java
+++ src/test/java/AbilityTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AbilityTest {
@@ -9,14 +8,14 @@ public class AbilityTest {
     @DisplayName("Returns if it is a Magic Ability")
     public void testOnlyMagicAbility() {
         MagicAbility magicAbility = new MagicAbility("Magic");
-        assertEquals("Magic", magicAbility.typeOfAbility());
+        assertEquals("Magic", magicAbility.toString());
     }
 
     @Test
     @DisplayName("Returns if it is a Physical Ability")
     public void testOnlyPhysicalAbility() {
         PhysicalAbility physicalAbility = new PhysicalAbility("Physical");
-        assertEquals("Physical", physicalAbility.typeOfAbility());
+        assertEquals("Physical", physicalAbility.toString());
     }
 
     @Test
@@ -24,6 +23,7 @@ public class AbilityTest {
     public void testBothMagicAndPhysicalAbility() {
         MagicAbility magicAbility = new MagicAbility("Magic");
         PhysicalAbility physicalAbility = new PhysicalAbility("Physical");
-        assertEquals("Magic" + "Physical", magicAbility.typeOfAbility() + physicalAbility.typeOfAbility());
+        assertEquals("Magic" + "Physical", magicAbility.toString() + physicalAbility.toString());
     }
+
 }
