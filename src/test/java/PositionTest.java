d5b1e1265ab4fd56466fc811c2cd89b0ec1b0a6e
KrilleBoi <kristian.remnelius@gmail.com>
2023-10-10 11:26:47 +0200

    Added some tests to PositionTest

--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -1,8 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PositionTest {
 
@@ -33,8 +32,25 @@ public class PositionTest {
     @DisplayName("assert two Positions where p1.x == p2.y and p1.y == p2.x returns different HashCodes")
     void assertDifferentHashCodes() {
         Position pos1 = new Position(DEFAULT_POSITIVE_X, DEFAULT_Y);
+        //Intentionally placing Y-value into X and vice versa, for testings' sake
         Position pos2 = new Position(DEFAULT_Y, DEFAULT_POSITIVE_X);
         assertNotEquals(pos1.hashCode(), pos2.hashCode());
+    }
+
+    @Test
+    @DisplayName("assert two positions with same X&Y equals true")
+    void assertEqualsMethodSameValues() {
+        Position pos1 = new Position(DEFAULT_NEGATIVE_X, DEFAULT_NEGATIVE_Y);
+        Position pos2 = new Position(DEFAULT_NEGATIVE_X, DEFAULT_NEGATIVE_Y);
+        assertEquals(true, pos1.equals(pos2));
+    }
+
+    @Test
+    @DisplayName("assert two positions with different X&Y equals false")
+    void assertEqualsMethodDifferentValues() {
+        Position pos1 = new Position(DEFAULT_NEGATIVE_X, DEFAULT_NEGATIVE_Y);
+        Position pos2 = new Position(DEFAULT_POSITIVE_X, DEFAULT_NEGATIVE_Y);
+        assertEquals(false, pos1.equals(pos2));
     }
 
 
