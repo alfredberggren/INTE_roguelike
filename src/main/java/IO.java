8927fc1bda4e570c4f74909269295de8ec2110ab
Simon <simon0285@gmail.com>
2023-10-13 14:14:42 +0200

    changed so the test test IO instead of TextUI and added the IO methods as abstract in IO class so they go through

--- src/main/java/IO.java
+++ src/main/java/IO.java
@@ -2,4 +2,16 @@ public abstract class IO {
     public enum TurnCommand{
         ACTION, MOVE, END  
     }
+
+    public abstract TurnCommand requestTurnCommand(Room room, Character character);
+
+    public abstract TurnCommand requestAnotherTurnCommand(Room room, Character character);
+
+    public abstract CardinalDirection requestMove(Room room, Character character);
+
+    public abstract CardinalDirection requestAnotherMove(Room room, Character character);
+
+    public abstract Interactable.InteractableAction requestAction(Room room, Character character);
+
+    public abstract Interactable.InteractableAction requestAnotherAction(Room room, Character character);
 }
