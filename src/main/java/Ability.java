f9cb1545ab4f5398b9cc867cfaafc50f2d785a14
annastrombeerg <annastromberg03@yahoose>
2023-10-14 14:02:44 +0200

    La till JavaDoc kommentarer

--- src/main/java/Ability.java
+++ src/main/java/Ability.java
@@ -1,4 +1,5 @@
-
+/**The Ability class represents an abstract concept of character abilities
+ * It provides a foundation for creating various types of abilities */
 public abstract class Ability {
 
     protected String name;
@@ -6,26 +7,33 @@ public abstract class Ability {
     protected AbilityType type;
     protected int minimumLevel;
 
+    /**Constructs an Ability object with the specified characteristics
+     * such as name, base damage, minimum level to retain and type of ability */
     public Ability(String name, int baseDamage, AbilityType type, int minimumLevel) {
         this.name = name;
         this.baseDamage = baseDamage;
         this.minimumLevel = minimumLevel;
         this.type = type;
     }
+
+    /**Calculates the damage inflicted by this ability
+     * when used by a character. */
     abstract public int calculateDamageOfAbility(Character character);
 
+    /**Representing the type of ability*/
     public enum AbilityType {
         PHYSICAL, MAGICAL
     }
-
+    /**Gets the type of ability*/
     public AbilityType getTypeOfAbility(){
         return type;
     }
-
+    /**Checks whether the ability is learnable by a character
+     * based on their level*/
     public boolean isLearnable(Character character) {
         return character.getLevel() >= minimumLevel;
     }
-
+    /**Returns a string representation of the ability, its name*/
     abstract public String toString();
 
 }
--- src/test/java/InteractableInventoryTest.java
+++ src/test/java/InteractableInventoryTest.java
@@ -12,9 +12,9 @@ public class InteractableInventoryTest {
     static final Set<Interactable.InteractableAction> DEFAULT_NPC_ACTIONS = new HashSet<>(Arrays.asList(Interactable.InteractableAction.FIGHT, Interactable.InteractableAction.TALK));
     static final Set<Interactable.InteractableAction> DEFAULT_EQUIPMENT_ACTIONS = new HashSet<>(Arrays.asList(Interactable.InteractableAction.LOOT, Interactable.InteractableAction.DROP));
 
-    static final NPC DEFAULT_NPC = new NPC("Harald", 100, 50, DEFAULT_NPC_ACTIONS);
+    static final NPC DEFAULT_NPC = new NPC("Harald", 100, 50, 100 ,DEFAULT_NPC_ACTIONS);
 
-    static final Equipment DEFAULT_EQUIPMENT = new Equipment("Sword", DEFAULT_EQUIPMENT_ACTIONS, Equipment.Effect.DAMAGE, 40, new PhysicalAbility("Slash",10,"Physical"));
+    static final Equipment DEFAULT_EQUIPMENT = new Equipment("Sword", DEFAULT_EQUIPMENT_ACTIONS, Equipment.Effect.DAMAGE, 40, new PhysicalAbility("Slash",10,1));
 
     InteractableInventory inventory;
 
@@ -22,7 +22,7 @@ public class InteractableInventoryTest {
     @Test
     public void testAddMethodAddsInteractable(){
         inventory = new InteractableInventory();
-        Equipment e = new Equipment("Axe", DEFAULT_EQUIPMENT_ACTIONS, Equipment.Effect.DAMAGE, 20, new PhysicalAbility("Slash",10,"Physical"));
+        Equipment e = new Equipment("Axe", DEFAULT_EQUIPMENT_ACTIONS, Equipment.Effect.DAMAGE, 20, new PhysicalAbility("Slash",10,1));
         inventory.add(e);
         assertEquals(true, inventory.contains(e));
     }
@@ -53,7 +53,7 @@ public class InteractableInventoryTest {
     public void testTransferMethodDoesNotTransferWhenInteractableNotInInventory(){
         setUpDefaultInventory();
         InteractableInventory inventory2 = new InteractableInventory();
-        Equipment e = new Equipment("Potion", DEFAULT_EQUIPMENT_ACTIONS, Equipment.Effect.HEALTH, 0, new MagicAbility("Heal",10,"Magic"));
+        Equipment e = new Equipment("Potion", DEFAULT_EQUIPMENT_ACTIONS, Equipment.Effect.HEALTH, 0, new MagicAbility("Heal",10,1));
         inventory.transfer(e, inventory2);
         assertEquals(false, inventory2.contains(DEFAULT_EQUIPMENT));
     }
