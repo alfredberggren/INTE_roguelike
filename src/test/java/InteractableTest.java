1e62453f29f9e4af918c9ea5584c4d6299cdb3a9
Alfred Berggren <alfred.berggren@gmail.com>
2023-10-16 11:15:38 +0200

    Classes that extend character no longer support customized actions, so they were removed from the initializations.

--- src/test/java/InteractableTest.java
+++ src/test/java/InteractableTest.java
@@ -26,7 +26,7 @@ public class InteractableTest {
     @DisplayName("Testar en NPC-karaktärs actions")
     public void testNPCHasCorrectActions() {
         Set<Interactable.InteractableAction> possibleInteractableActions = new HashSet<>(Arrays.asList(Interactable.InteractableAction.FIGHT, Interactable.InteractableAction.TALK));
-        Interactable i1 = new NPC("Fido", 10, 100,10, possibleInteractableActions);
+        Interactable i1 = new NPC("Fido", 10, 100,10);
         assertEquals(possibleInteractableActions, i1.getPossibleActions());
     }
 
@@ -81,24 +81,24 @@ public class InteractableTest {
     @Test
     @DisplayName("Två NPC:s med samma namn ska vara lika")
     public void testTwoNPCsWithSameNameAreEqual() {
-        assertEquals(new NPC("test123", 10, 10, 10, DEFAULT_INTERACTABLE_ACTIONS), new NPC("test123", 13, 23,10, DEFAULT_INTERACTABLE_ACTIONS));
+        assertEquals(new NPC("test123", 10, 10, 10), new NPC("test123", 13, 23,10));
     }
 
     @Test
     @DisplayName("Två NPC:s med samma namn ska ha samma hashCode")
     public void testTwoNPCsWithSameNameShouldHaveSameHashCode() {
-        assertEquals(new NPC("test123", 10, 10, 10,DEFAULT_INTERACTABLE_ACTIONS).hashCode(), new NPC("test123", 13, 23,10, DEFAULT_INTERACTABLE_ACTIONS).hashCode());
+        assertEquals(new NPC("test123", 10, 10, 10).hashCode(), new NPC("test123", 13, 23,10).hashCode());
     }
 
     @Test
     @DisplayName("Två NPCs som är olika")
     public void testTwoUnequalNPCs() {
-        assertNotEquals(new NPC("test1234", 10, 10, 10,DEFAULT_INTERACTABLE_ACTIONS), new NPC("test123", 13, 23,10, DEFAULT_INTERACTABLE_ACTIONS));
+        assertNotEquals(new NPC("test1234", 10, 10, 10), new NPC("test123", 13, 23,10));
     }
 
     @Test
     @DisplayName("Två olika NPCs har olika hashCode")
     public void testTwoUnequalNPCsShouldHaveUnequalHashCodes() {
-        assertNotEquals(new NPC("test1234", 10, 10, 10,DEFAULT_INTERACTABLE_ACTIONS).hashCode(), new NPC("test123", 13, 23,10, DEFAULT_INTERACTABLE_ACTIONS).hashCode());
+        assertNotEquals(new NPC("test1234", 10, 10, 10).hashCode(), new NPC("test123", 13, 23,10).hashCode());
     }
 }
