66cc9aa0798bdeb4a3f65fd11abbdad0cc54ed4b
Alfred Berggren <alfred.berggren@gmail.com>
2023-10-16 11:18:48 +0200

    Removed old, commented code

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -1,15 +1,5 @@
 public class GameController {
-//    private MapController mapController;
-//    private Difficulty difficulty;
-//    private int amountOfRooms;
-//    private Player player;
-//    private MapBuilder mapBuilder;
     public GameController() {
-        //difficulty = IO.promptDifficulty();
-        //amountOfRooms = IO.promptAmountOfRooms();
-        //character = IO.promptCreatePlayer();
-        //mapController = new MapController();
-        //mapBuilder = new MapBuilder(difficulty, amountOfRooms, player, mapController);
     }
 
     public static void main(String[] args) {
