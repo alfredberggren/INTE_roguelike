31c2eb6f22e2d421ce9bbf9f408452cac3274749
palinaboczkowska <palina.boczkowska@gmail.com>
2023-10-17 10:24:39 +0200

    fixade ordning av metoder i Character.java

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -12,9 +12,7 @@ public class Character implements Interactable {
             InteractableAction.FIGHT)
     );
     private static final String NAME_PATTERN = "^[A-Za-z]\\w{1,11}$";
-
     private Set<InteractableAction> possibleInteractableActions;
-
     protected String name;
     private int health;
     private int speed;
@@ -60,31 +58,6 @@ public class Character implements Interactable {
         possibleInteractableActions = STANDARD_CHARACTER_INTERACTABLE_ACTIONS;
         turnSystem = new TurnSystem(io);
     }
-
-    public void setName(String name) {
-        /** user input
-         the username consists of 2 to 10 characters. If less - invalid username
-         the username can only contain alphanumeric characters and underscores(_)
-         uppercase, lowercase and digits (0-9)
-         the first character must be an alphabetic character*/
-        if (name == null) {
-            throw new NullPointerException("Error: name can't be null");
-        }
-        if (name.isEmpty()) {
-            throw new IllegalArgumentException("Error: name can't be empty");
-        }
-        if (!matchesPattern(NAME_PATTERN, name)) {
-            throw new IllegalArgumentException("Name doesn't match the pattern");
-        } else
-            this.name = name;
-    }
-
-    boolean matchesPattern(String pattern, String name) {
-        Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(name);
-        return m.find();
-    }
-
     public String getName() {
         return name;
     }
@@ -119,6 +92,30 @@ public class Character implements Interactable {
 
     public boolean getSpell() {
         return spell;
+    }
+
+    public void setName(String name) {
+        /** user input
+         the username consists of 2 to 10 characters. If less - invalid username
+         the username can only contain alphanumeric characters and underscores(_)
+         uppercase, lowercase and digits (0-9)
+         the first character must be an alphabetic character*/
+        if (name == null) {
+            throw new NullPointerException("Error: name can't be null");
+        }
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("Error: name can't be empty");
+        }
+        if (!matchesPattern(NAME_PATTERN, name)) {
+            throw new IllegalArgumentException("Name doesn't match the pattern");
+        } else
+            this.name = name;
+    }
+
+    boolean matchesPattern(String pattern, String name) {
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(name);
+        return m.find();
     }
 
     /**Checks if Arraylist is not empty and if so removes the spell*/
